7986928e13e6cf6babc04a2dbe15bb0644df50e6
gmaillet <gregoire.maillet@gmail.com>
2017-05-24 00:21:03 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
+/*
 #include <iostream>
 
 int main(int argc, char **argv)
 {
         std::cout << "TEST"<<std::endl;
         return 0;
+}
+*/
+#include <QApplication>
+#include <QTextEdit>
+
+int main(int argv, char **args)
+{
+        QApplication app(argv, args);
+        QTextEdit textEdit;
+        textEdit.show();
+        return app.exec();
 }
 
